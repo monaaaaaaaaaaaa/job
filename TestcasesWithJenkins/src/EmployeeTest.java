493e03729196bd0463854f21f7a236d56b199019
monaaaaaaaaaaaaa <mohanarubarajamohan@gmail.com>
2021-08-24 15:51:18 +0530

    First Commit

---
+++ TestcasesWithJenkins/src/EmployeeTest.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class EmployeeTest {
+
+	static HashMap<Integer, String> emp = new HashMap<Integer, String>();
+
+	public static String addEmp(int empid, String empname) {
+		String res = emp.put(empid, empname);
+		//System.out.println(res);
+		return res;
+	}
+
+	public static String updateEmp(int empid, String empname) {
+		String res = emp.put(empid, empname);
+		System.out.println(res);
+		return res;
+	}
+
+	public static String deleteEmp(int empid) {
+		String res = emp.remove(empid);
+		System.out.println(res);
+		return res;
+	}
+
+	public static String fetchEmp(int empid) {
+		String res = emp.get(empid);
+		System.out.println(res);
+		return res;
+	}
+
+	public static int fetchAllEmps() {
+		return emp.size();
+	}
+
+}
---
+++ TestcasesWithJenkins/src/TestCasesEmp.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+
+public class TestCasesEmp {
+
+	@Before
+	public void BeforeAll() {
+		System.out.println("Started executing testcases for employee testing");
+	}
+	@After
+	public void AfterAll() {
+		System.out.println("Execution Completed");
+	}
+	@Test
+	public void allOps() {
+		String res=EmployeeTest.addEmp(1, "mona");
+		String res1=EmployeeTest.addEmp(2,"ramya");
+		String res2=EmployeeTest.addEmp(3,"nathira");
+		String upd=EmployeeTest.updateEmp(2,"divya");
+		String fetch=EmployeeTest.fetchEmp(3);
+		int size=EmployeeTest.fetchAllEmps();
+		String del=EmployeeTest.deleteEmp(3);
+		int size1=EmployeeTest.fetchAllEmps();
+		assertEquals(null,res);
+		assertEquals("ramya",upd);
+		assertEquals("nathira",fetch);
+		assertEquals(3,size);
+		assertEquals("nathira",del);
+		assertNotEquals(4,size1);
+	}
+	@Ignore
+	public void add() {
+		String res=EmployeeTest.addEmp(5, "mona");
+		assertEquals("mona",res);
+	}
+}
