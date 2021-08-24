189971b4289d69aeb2874d8976886cfd13ac91fb
monaaaaaaaaaaaaa <mohanarubarajamohan@gmail.com>
2021-08-24 15:30:07 +0530

    second commit

--- SpringJenkins/src/main/java/com/example/demo/SpringJenkinsApplication.java
+++ SpringJenkins/src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+		System.out.println("hey");
 	}
 
 }
